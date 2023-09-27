cd207a079b450ca25690a7829a06272d3675493b
thesimplezero <hi@thesimplezero.io>
2023-09-27 16:35:56 +0300

    corrections

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,59 +1,40 @@
-# Add the stable version of Nginx repository
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+#!/usr/bin/env bash
+# Configure server using puppet
 
-# Update the list of software packages
-exec { 'update packages':
-  command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+# Defines Puppet class called nginx_server that 
+# Encapsulates configuration for Nginx server.
+class nginx_server {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-# Install Nginx
-package { 'nginx':
-  ensure => 'installed',
-}
+  # Manages Nginx service.
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    require => Package['nginx'],
+  }
+ # Manages Nginx configuration file located /etc/nginx/sites-available/default.
+  file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => "
+      server {
+        listen      80 default_server;
+        listen      [::]:80 default_server;
+        root        /var/www/html;
+        index       index.html index.htm;
 
-# Allow HTTP traffic through UFW
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
-}
+        location / {
+          return 200 'Hello World!';
+        }
 
-# Change folder permissions to 755 for /var/www
-exec { 'chmod www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+        location /redirect_me {
+          return 301 http://youtube.com/;
+        }
+      }
+    ",
+    notify => Service['nginx'],
+  }
 }
-
-# Create an index.html file with "Hello World!" content
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-}
-
-# Create a 404.html file with custom content
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-# Configure Nginx with custom settings including redirection and error page
-file { 'Nginx default config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  content =>
-    # Nginx server block configuration here
-}
-
-# Restart the Nginx service
-exec { 'restart service':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
+# Includes nginx_server class, ensuring it gets applied.
+include nginx_server
